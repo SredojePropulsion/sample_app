678948ed171301280055f7a8bed374e5bfcaa539
SredojePropulsion <sredoje.cutovic@propulsionapp.com>
2015-02-09 08:59:24 +0100

    Added update/delete users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,8 @@ class User < ActiveRecord::Base
 	validates :name, presence: true, length: { maximum: 50 }
 	validates :email, presence:true, length: { maximum: 255 },
 	format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
-	validates :password, length: { minimum: 6 }
+	validates :password, length: { minimum: 6 }, allow_blank: true
 	has_secure_password
-
 	def User.digest(string)
 	    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
 	                                                  BCrypt::Engine.cost
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150204074344) do
+ActiveRecord::Schema.define(version: 20150206084817) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "password_digest"
     t.string   "remember_digest"
+    t.boolean  "admin",           default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
