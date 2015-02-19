a5e8975af83fff155ae38bbcc8533d0837948eeb
SredojePropulsion <sredoje.cutovic@propulsionapp.com>
2015-02-19 08:47:51 +0100

    Finished rails

--- Gemfile
+++ Gemfile
@@ -12,6 +12,9 @@ gem 'jquery-rails',         '4.0.3'
 gem 'turbolinks',           '2.3.0'
 gem 'jbuilder',             '2.2.3'
 gem 'bcrypt',               '3.1.7'
+gem 'carrierwave',          '0.10.0'
+gem 'mini_magick',          '3.8.0'
+gem 'fog',                  '1.23.0'
 gem 'sdoc',                 '0.4.0', group: :doc
 
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,24 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy,
+                                        :following, :followers]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
 
-  
+
+  def following
+    @title = "Following"
+    @user  = User.find(params[:id])
+    @users = @user.following.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user  = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
   def index
     @users = User.paginate(page: params[:page])
   end
@@ -12,6 +27,7 @@ class UsersController < ApplicationController
   end
   def show
   	@user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
   def create
     @user = User.new(user_params)    # Not the final implementation!
@@ -46,14 +62,7 @@ class UsersController < ApplicationController
 	def user_params
 		 params.require(:user).permit(:name, :email, :password, :password_confirmation)
 	end
-  # Confirms a logged-in user.
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in."
-      redirect_to login_url
-    end
-  end
+
   # Confirms the correct user.
   def correct_user
     @user = User.find(params[:id])
